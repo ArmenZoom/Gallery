02e754fffcd41378b895002a375b34904de29fa0
Armen Aleksanyan <arm1992en@mail.ru>
2019-11-07 17:29:52 +0400

    sort gallery

--- Sources/Videos/VideosLibrary.swift
+++ Sources/Videos/VideosLibrary.swift
@@ -23,20 +23,27 @@ class VideosLibrary {
     }
   }
 
-  fileprivate func reloadSync() {
-    fetchResults = PHAsset.fetchAssets(with: .video, options: Utils.fetchOptions())
-    items = []
-    fetchResults?.enumerateObjects({ (asset, _, _) in
-      self.items.append(Video(asset: asset))
-    })
-    
-    if Config.tabsToShow == [.videoImageTab] {
-        let imagesResault = PHAsset.fetchAssets(with: .image, options: nil)
-          imagesResault.enumerateObjects { (asset, _, _) in
-              self.items.append(Video(asset: asset, isVideo: false))
-          }
+  
+    fileprivate func reloadSync() {
+        fetchResults = PHAsset.fetchAssets(with: .video, options: Utils.fetchOptions())
+        items = []
+        fetchResults?.enumerateObjects({ (asset, _, _) in
+          self.items.append(Video(asset: asset))
+        })
+        
+        if Config.tabsToShow == [.videoImageTab] {
+            let imagesResault = PHAsset.fetchAssets(with: .image, options: nil)
+              imagesResault.enumerateObjects { (asset, _, _) in
+                  self.items.append(Video(asset: asset, isVideo: false))
+              }
+        }
+        self.items.sort { (item1, item2) -> Bool in
+            let date1 = item1.asset.creationDate ?? Date()
+            let date2 = item2.asset.creationDate ?? Date()
+            return  date1 > date2
+        }
+
     }
-  }
     
 }
 
