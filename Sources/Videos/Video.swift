d9bb101481b69a3c3a207d42f2745deb7338ef94
Armen Aleksanyan <arm1992en@mail.ru>
2020-04-07 15:54:30 +0400

    add fetchURL completion URL

--- Sources/Videos/Video.swift
+++ Sources/Videos/Video.swift
@@ -142,6 +142,38 @@ public class Video: Equatable {
              }
          }
     }
+    
+    public func fetchURL(completion: @escaping (_ url: URL?) -> Void) {
+        self.getURL(ofPhotoWith: asset) { (url) in
+            completion(url)
+        }
+    }
+    
+    private func getURL(ofPhotoWith mPhasset: PHAsset, completionHandler : @escaping ((_ responseURL : URL?) -> Void)) {
+        if mPhasset.mediaType == .image {
+            let options: PHContentEditingInputRequestOptions = PHContentEditingInputRequestOptions()
+            options.canHandleAdjustmentData = {(adjustmeta: PHAdjustmentData) -> Bool in
+                return true
+            }
+            mPhasset.requestContentEditingInput(with: options, completionHandler: { (contentEditingInput, info) in
+                completionHandler(contentEditingInput!.fullSizeImageURL)
+            })
+        } else if mPhasset.mediaType == .video {
+            let options: PHVideoRequestOptions = PHVideoRequestOptions()
+            options.version = .original
+            PHImageManager.default().requestAVAsset(forVideo: mPhasset, options: options, resultHandler: { (asset, audioMix, info) in
+                if let urlAsset = asset as? AVURLAsset {
+                    let localVideoUrl = urlAsset.url
+                    completionHandler(localVideoUrl)
+                } else {
+                    completionHandler(nil)
+                }
+            })
+        }
+        
+    }
+    
+    
 
   // MARK: - Helper
 
