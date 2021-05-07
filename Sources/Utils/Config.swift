a458a9f3d5ce9166e024003c9fadba836d04ebff
Armen Aleksanyan <arm1992en@mail.ru>
2021-05-07 17:45:58 +0400

    change default font

--- Sources/Utils/Config.swift
+++ Sources/Utils/Config.swift
@@ -167,6 +167,6 @@ public struct Config {
     
     public struct DropDown {
         public static var isEnabled: Bool = false
-        public static var textFont: UIFont = UIFont.boldSystemFont(ofSize: 16)
+        public static var textFont: UIFont = UIFont.boldSystemFont(ofSize: 14)
     }
 }
